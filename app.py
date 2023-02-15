2eb2884902abd0bf9c0066bf27a21c89e56c6e62
Mohammed Osman Mahmoud Bahageel <119727641+Datascientist88@users.noreply.github.com>
2023-02-15 16:31:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("../main/data").resolve()
 
 
-df=pd.read_excel(DATA_PATH.joinpath(""))
+df=pd.read_excel(DATA_PATH.joinpath("unemplo_figures_1991.xlsx"))
 
 YEARS=[2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
        2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,

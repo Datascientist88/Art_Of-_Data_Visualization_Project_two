42eb7b45b84ef540536996f88ee83c5cc06152a9
Mohammed Osman Mahmoud Bahageel <119727641+Datascientist88@users.noreply.github.com>
2023-02-15 16:07:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ import dash_bootstrap_components as dbc
 import plotly_express as px
 from plotly.subplots import make_subplots
 from dash import dcc, html, callback, Output
-from pathlib import Path 
+import pathlib
 
 # load the data ------------------------------------------------------------------------
-path=Path("data")/"unemplo_figures_1991.xlsx"
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("../data").resolve()
 
-df=pd.read_excel(path,error_bad_lines = False,engine="openpyxl")
+
+df=pd.read_excel(DATA_PATH.joinpath("unemplo_figures_1991.xlsx"))
 
 YEARS=[2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
        2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,

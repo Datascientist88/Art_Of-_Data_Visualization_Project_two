258b811f18b4425760613e3d0f96c37eaf13710a
Mohammed Osman Mahmoud Bahageel <119727641+Datascientist88@users.noreply.github.com>
2023-02-15 15:48:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from dash import dcc, html, callback, Output
 
 # load the data ------------------------------------------------------------------------
 
-df=pd.read_csv("https://github.com/Datascientist88/Data_store/blob/main/project_interactive_map/unemplo_figures_1991.csv")
+df=pd.read_csv("https://github.com/Datascientist88/Data_store/blob/main/project_interactive_map/unemplo_figures_1991.csv",error_bad_lines = False)
 
 YEARS=[2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
        2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,

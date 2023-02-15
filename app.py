d68a6a5297a273e3cc9cc16e1bd79345ef4effa1
Mohammed Osman Mahmoud Bahageel <119727641+Datascientist88@users.noreply.github.com>
2023-02-15 15:58:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,12 @@ import dash_bootstrap_components as dbc
 import plotly_express as px
 from plotly.subplots import make_subplots
 from dash import dcc, html, callback, Output
+from pathlib import Path 
 
 # load the data ------------------------------------------------------------------------
+path=Path("data")/""
 
-df=pd.read_csv("https://github.com/Datascientist88/Data_store/blob/main/project_interactive_map/unemplo_figures_1991.csv",error_bad_lines = False)
+df=pd.read_excel("https://github.com/Datascientist88/Data_store/blob/main/project_interactive_map/unemplo_figures_1991.csv",error_bad_lines = False)
 
 YEARS=[2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
        2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
